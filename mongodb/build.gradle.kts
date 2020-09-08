6e4d11350ad786331544b5cc77bf5768e29e50ef
Adrian Cole <adrian.f.cole@gmail.com>
2020-09-08 16:24:22 +0800

    newer plugin

--- mongodb/build.gradle.kts
+++ mongodb/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.hypertrace.docker-plugin") version "0.6.1"
-  id("org.hypertrace.docker-publish-plugin") version "0.6.1"
+  id("org.hypertrace.docker-plugin") version "0.7.0"
+  id("org.hypertrace.docker-publish-plugin") version "0.7.0"
 }
 
 hypertraceDocker {
