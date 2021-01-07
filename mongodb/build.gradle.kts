0574c31b9fd5416bfb53db9e96e0ef802b56f658
Jayesh Bapu Ahire <jayeshbahire@programmer.net>
2021-01-07 12:30:33 +0530

    adds-gha

--- mongodb/build.gradle.kts
+++ mongodb/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.hypertrace.docker-plugin") version "0.7.0"
-  id("org.hypertrace.docker-publish-plugin") version "0.7.0"
+  id("org.hypertrace.docker-plugin") version "0.8.1"
+  id("org.hypertrace.docker-publish-plugin") version "0.8.1"
 }
 
 hypertraceDocker {
