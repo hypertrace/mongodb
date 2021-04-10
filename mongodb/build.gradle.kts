ce42c1b796f3c6f29a505f6be18e0547683e6a4b
Ravi Singal <ravi.singal@traceable.ai>
2021-04-10 11:41:52 +0530

    publish artifacts to hypertrace artifactory

--- mongodb/build.gradle.kts
+++ mongodb/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.hypertrace.docker-plugin") version "0.8.1"
-  id("org.hypertrace.docker-publish-plugin") version "0.8.1"
+  id("org.hypertrace.docker-plugin") version "0.9.0"
+  id("org.hypertrace.docker-publish-plugin") version "0.9.0"
 }
 
 hypertraceDocker {
