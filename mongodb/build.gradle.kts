c4c7acb7609a3eb7e66d592d3f872794f397c221
Senan Jude <senanjude@Senans-MacBook-Pro.local>
2022-07-20 15:32:26 +0530

    changed docker-plugin version

--- mongodb/build.gradle.kts
+++ mongodb/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("org.hypertrace.docker-plugin") version "0.9.0"
+  id("org.hypertrace.docker-plugin") version "0.9.4"
   id("org.hypertrace.docker-publish-plugin") version "0.9.4"
 }
 
