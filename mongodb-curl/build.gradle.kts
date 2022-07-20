5f04a35a4b8f45a05a4713fcf4f7589141959c68
Senan Jude <senanjude@Senans-MacBook-Pro.local>
2022-07-20 12:47:32 +0530

    added container scanning to mongodb repo

--- mongodb-curl/build.gradle.kts
+++ mongodb-curl/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("org.hypertrace.docker-plugin") version "0.9.0"
-  id("org.hypertrace.docker-publish-plugin") version "0.9.0"
+  id("org.hypertrace.docker-publish-plugin") version "0.9.4"
 }
 
 var mongoVersion = "4.4.0"
