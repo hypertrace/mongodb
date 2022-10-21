0a72cd835a34099eff4d87667dd4d34e8e92d71d
Ravi Singal <ravi.singal@traceable.ai>
2022-10-21 17:59:14 +0530

    upgrade mongodb version to 4.4.17

--- mongodb-backup/build.gradle.kts
+++ mongodb-backup/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("org.hypertrace.docker-plugin") version "0.9.0"
-  id("org.hypertrace.docker-publish-plugin") version "0.9.0"
+  id("org.hypertrace.docker-plugin") version "0.9.9"
+  id("org.hypertrace.docker-publish-plugin") version "0.9.9"
 }
 
 hypertraceDocker {
--- mongodb-curl/build.gradle.kts
+++ mongodb-curl/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-  id("org.hypertrace.docker-plugin") version "0.9.0"
-  id("org.hypertrace.docker-publish-plugin") version "0.9.0"
+  id("org.hypertrace.docker-plugin") version "0.9.9"
+  id("org.hypertrace.docker-publish-plugin") version "0.9.9"
 }
 
-var mongoVersion = "4.4.0"
+var mongoVersion = "4.4.17"
 
 hypertraceDocker {
   defaultImage {
